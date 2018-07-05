91c2f36a6383e49d06215f7fd68e0d28bab4271c
Pankaj Doharey <pankajdoharey@gmail.com>
2018-07-06 04:54:50 +0530

    Clears screen and repositions cursor to 0,0 i.e Beginning of the screen.

--- pe.scm
+++ pe.scm
@@ -3,11 +3,20 @@
 (define (read-text)
   (let loop []
     (let [[key-in (read-char)]]
-     (display (format "~S " key-in))
+     (write-string (format "~S " key-in))
      (if (not (eq? key-in #\q))
 	 (loop)))))
 
+(define clear-screen "\x1b[2J")
+
+(define reposition-cursor "\x1b[H")
+
+(define (init-screen)
+  (write-string clear-screen)
+  (write-string reposition-cursor))
+
 (define (main)
+  (init-screen)
   (with-stty '(not echo icanon isig ixon icrnl opost) read-text))
 
 (main)
