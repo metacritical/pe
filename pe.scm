41f8d041b855668395bc5a6e9c0eac64270ceebc
Pankaj Doharey <pankajdoharey@gmail.com>
2018-07-06 05:19:20 +0530

    Refactored init-screen function ans setup exit routines.

--- pe.scm
+++ pe.scm
@@ -7,16 +7,19 @@
      (if (not (eq? key-in #\q))
 	 (loop)))))
 
-(define clear-screen "\x1b[2J")
-
-(define reposition-cursor "\x1b[H")
+(define (clear-screen) (write-string "\x1b[2J"))
+(define (reposition-cursor) (write-string "\x1b[H"))
 
 (define (init-screen)
-  (write-string clear-screen)
-  (write-string reposition-cursor))
+  (clear-screen)
+  (reposition-cursor))
+
+(define (setup-exit-routines)
+  (on-exit (lambda () (init-screen))))
 
 (define (main)
   (init-screen)
+  (setup-exit-routines)
   (with-stty '(not echo icanon isig ixon icrnl opost) read-text))
 
 (main)
