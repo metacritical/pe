475ea3e9fc5f0ed938ea4cb59f1893ea852c701f
Pankaj Doharey <pankajdoharey@gmail.com>
2018-07-10 10:52:21 +0530

    Added temporary text buffer which is printed once instead of multiple print calls. Also cursor hiding and show to remove flicker.

--- pe.scm
+++ pe.scm
@@ -6,27 +6,47 @@
      (if (not (eq? key-in #\q))
 	 (read-text))))
 
-(define text-buffer "")
+(define tmp-buffer "")
+(define (to<-tmpb str)
+  (set! tmp-buffer (string-append tmp-buffer str)))
+
+(define (clear-buffer buff)
+  (set! buff ""))
+
 (define screen-size (ioctl-winsize))
 (define clear-screen "\x1b[2J")
 (define reposition-cursor "\x1b[H")
 (define get-cursor-position "\x1b[6n")
+(define hide-cursor "\x1b[?25l")
+(define show-cursor "\x1b[?25h")
+
 (define (editor-draw-rows)
   (let [[rows (car screen-size)]]
     (let loop [[step 0] [str ""]]
-      (if (= step rows)
-	  (write-string str)
-	  (loop (+ step 1) (string-append str "~\r\n"))))))
+      (if (< step rows)
+	  (loop (+ step 1) (to<-tmpb "~\r\n"))))))
 
 (define (redraw-screen)
-  (write-string (string-append clear-screen reposition-cursor)))
+  (to<-tmpb (string-append hide-cursor clear-screen reposition-cursor)))
 
-(define (setup-exit-routines) (on-exit redraw-screen))
+(define (post-draw-routine)
+  (to<-tmpb (string-append hide-cursor reposition-cursor show-cursor)))
+
+(define (exit-routines)
+  (post-draw-routine)
+  (to<-tmpb clear-screen)
+  (reify-buffer tmp-buffer))
+
+(define (reify-buffer buff)
+  (write-string buff)
+  (clear-buffer buff))
+
+;;Init Exit Routines
+(on-exit exit-routines)
 
 ((lambda []
    (redraw-screen)
-   (setup-exit-routines)
    (editor-draw-rows)
-   (write-string get-cursor-position)
-   (write-string reposition-cursor)
+   (post-draw-routine)
+   (reify-buffer tmp-buffer)
    (with-stty '(not echo icanon isig ixon icrnl opost) read-text)))
