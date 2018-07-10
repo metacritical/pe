52d51ac23d4ce0bc2e423e1d931ca24c0ce95da7
Pankaj Doharey <pankajdoharey@gmail.com>
2018-07-10 11:03:21 +0530

    Renames Reposition cursor to reset cursor.

--- pe.scm
+++ pe.scm
@@ -15,7 +15,7 @@
 
 (define screen-size (ioctl-winsize))
 (define clear-screen "\x1b[2J")
-(define reposition-cursor "\x1b[H")
+(define reset-cursor "\x1b[H")
 (define get-cursor-position "\x1b[6n")
 (define hide-cursor "\x1b[?25l")
 (define show-cursor "\x1b[?25h")
@@ -27,13 +27,12 @@
 	  (loop (+ step 1) (to<-tmpb "~\r\n"))))))
 
 (define (redraw-screen)
-  (to<-tmpb (string-append hide-cursor clear-screen reposition-cursor)))
+  (to<-tmpb (string-append hide-cursor clear-screen reset-cursor show-cursor)))
 
 (define (post-draw-routine)
-  (to<-tmpb (string-append hide-cursor reposition-cursor show-cursor)))
+  (to<-tmpb (string-append hide-cursor reset-cursor show-cursor)))
 
 (define (exit-routines)
-  (post-draw-routine)
   (to<-tmpb clear-screen)
   (reify-buffer tmp-buffer))
 
