0a8bed08e84a73fc6e6b9aa023eeefcc4a3d0db6
Pankaj Doharey <pankajdoharey@gmail.com>
2018-07-29 20:30:07 +0530

    Adds gettr and settr for map attrs. Sets current buffer in global map.

--- core/init.scm
+++ core/init.scm
@@ -1,9 +1,22 @@
 (require-extension stty srfi-1 srfi-13)
 (require "core/keyboard.scm")
 
-(define (read-key)
-  (let [[key (read-char)]]
-    (handle-kb key)))
+(define tmp-buffer "")
+
+(define (to<-tmpb str)
+  (set! tmp-buffer (string-append tmp-buffer str)))
+
+(define (clear-buffer buff)
+  (set! buff ""))
+
+(define (exit-routines)
+  (to<-tmpb clear-screen)
+  (to<-tmpb reset-cursor)
+  (reify-buffer tmp-buffer))
+
+(define (reify-buffer buff)
+  (write-string buff)
+  (clear-buffer buff))
 
 (define (redraw-screen)
   (to<-tmpb (string-append hide-cursor clear-screen reset-cursor show-cursor)))
@@ -14,14 +27,35 @@
 (define (get-buffer-list)
   (filter (lambda (i) (eq? (car i) 'buffer-list)) global-map))
 
+(define (get-attr map-name attr)
+  (filter (lambda (i) (eq? (car i) attr)) map-name))
+
+(define (set-attr map-name key val)
+  (let [[curr-buff (get-attr map-name key)]]
+    (set! (cdar curr-buff) val)))
+
+(define (set-current-buffer buff)
+  (let [[name (get-attr buff 'name)]]
+    (set-attr global-map 'current-buffer (cdar name))))
+
 (define (generate-new-buffer)
   (let [[buff (new-buffer "New Buffer")][buff-lst (car (get-buffer-list))]]
-    (set! (cdr buff-lst) (cons buff (cdr buff-lst)))))
+    (set! (cdr buff-lst) (cons buff (cdr buff-lst)))
+    (set-current-buffer buff)))
 
+(define (read-key)
+  (let [[key (read-char)]]
+    (handle-kb key)))
+
+;;Init Exit Routines
+(on-exit exit-routines)
+
+;;Disable suspend and quit.
 (system "stty -ixon")
+
 (redraw-screen)
 (generate-new-buffer)
 (post-draw-routine)
 (reify-buffer tmp-buffer)
-;; (pp global-map)
+(pp global-map)
 (with-stty '(not echo icanon isig icrnl opost) read-key)
--- pe.scm
+++ pe.scm
@@ -2,14 +2,6 @@
 (require-extension srfi-14 ioctl extras utils)
 (use posix)
 
-(define tmp-buffer "")
-
-(define (to<-tmpb str)
-  (set! tmp-buffer (string-append tmp-buffer str)))
-
-(define (clear-buffer buff)
-  (set! buff ""))
-
 (define screen-size (ioctl-winsize))
 (define clear-screen "\x1b[2J")
 (define reset-cursor "\x1b[H")
@@ -17,15 +9,6 @@
 (define hide-cursor "\x1b[?25l")
 (define show-cursor "\x1b[?25h")
 (define (move-cursor-to x y) (format "\x1b[~S;~SH" y x))
-
-(define (exit-routines)
-  (to<-tmpb clear-screen)
-  (to<-tmpb reset-cursor)
-  (reify-buffer tmp-buffer))
-
-(define (reify-buffer buff)
-  (write-string buff)
-  (clear-buffer buff))
 
 (define (new-buffer name)
   (list (cons 'name name)
@@ -37,6 +20,7 @@
 
 (define global-map
   (list (cons 'buffer-list (list (new-buffer "*scratch*")))
+	(cons 'current-buffer "")
 	(cons 'frames '())
 	(cons 'windows '())
 	(cons 'mode-line '())
@@ -48,9 +32,6 @@
 
 (define (init-global-map)
   (set! global-map "Set buffer name etc..."))
-
-;;Init Exit Routines
-(on-exit exit-routines)
 
 ;;Init
 (require "core/init.scm")
